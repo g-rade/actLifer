46d6deb748be9733c3dafe04deb29b5cef324621
g-rade <grade@smith.edu>
2022-11-03 10:39:45 -0400

    11/3/2022: starting the central death rate and conditional probability of death functions

---
+++ R/Steps1_3.R
@@ -0,0 +1,23 @@
+#' Central Death Rate
+#'
+#' @param data The mortality dataset, includes an age grouping variable,
+#' @param age The age grouping variable, must be categorical
+#' @param pop Population of each age group, must be numeric
+#' @param deaths The midyear number of deaths at each age group, must be numeric
+#' @param ... Other optional grouping variables (can be race, gender, etc._)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+central_death_rate <- function(data, age, pop, deaths, ...){
+  data %>%
+    group_by(age, ...) %>%
+    mutate(CentralDeathRate = (deaths/pop))
+}
+
+conditional_death_prob <- function (data, age, pop, deaths, ...){
+  data %>%
+    group_by(age, ...) %>%
+
+}
