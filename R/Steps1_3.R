35e952cb5e44e06b37cf18298e910fad54276831
mctp546 <mtylerpenny@smith.edu>
2022-11-10 10:26:54 -0500

    11/10/22: test data and importing dplyr

--- R/Steps1_3.R
+++ R/Steps1_3.R
@@ -1,25 +1,64 @@
 #' Central death rate
 #'
+#' @param df Data frame
+#' @param age Age groups - column of data frame
 #' @param deaths Number of deaths in each age group - column of a data frame
 #' @param pop Population of each age group - column of a data frame
 #'
-#' @return central death rate
+#' @return Death rate (double)
 #' @export
-#'
+#' @import dplyr
 #' @examples
-death_rate <- function(deaths, pop) {
+death_rate <- function(df, age, deaths, pop) {
   return(deaths/pop)
 }
 
 #' Conditional probability of death
 #'
-#' @param deaths
-#' @param pop
+#' @param df Data frame
+#' @param age Age groups - column of data frame
+#' @param deaths Number of deaths in each age group - column of a data frame
+#' @param pop Population of each age group - column of a data frame
 #'
-#' @return
+#' @return conditional probability of death (double)
 #' @export
 #'
 #' @examples
-cp_death <- function(deaths, pop) {
+cp_death <- function(df, age, deaths, pop) {
   return(deaths/(pop + 0.5*deaths))
+}
+
+#' Conditional probability of survival
+#'
+#' @param df Data frame
+#' @param age Age groups - column of data frame
+#' @param deaths Number of deaths in each age group - column of a data frame
+#' @param pop Population of each age group - column of a data frame
+#'
+#' @return conditional probability of survival (double)
+#' @export
+#'
+#' @examples
+cp_survival <- function(df, age, deaths, pop) {
+  return(1 - (deaths/(pop + 0.5*deaths)))
+}
+
+
+
+#' Making the lifetable
+#'
+#' @param df
+#' @param age
+#' @param pop
+#' @param deaths
+#'
+#' @return
+#' @export
+#' @import dplyr
+#' @examples
+makes_lifetable <- function(df, age, pop, deaths) {
+  lifetable <- df %>%
+    mutate(death_rate = death_rate(),
+           cp_death = cp_death(),
+           cp_survival = cp_survival())
 }
