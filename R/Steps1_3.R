0e5da816580e9883de64bcad11290511a1b96eb5
mctp546 <mtylerpenny@smith.edu>
2022-11-14 15:02:47 -0500

    11/14: add step 4 and edits to make_lifetable and cp_survival

--- R/Steps1_3.R
+++ R/Steps1_3.R
@@ -28,6 +28,7 @@ cp_death <- function(df, age, deaths, pop) {
   return(deaths/(pop + 0.5*deaths))
 }
 
+
 #' Conditional probability of survival
 #'
 #' @param df Data frame
@@ -40,10 +41,26 @@ cp_death <- function(df, age, deaths, pop) {
 #'
 #' @examples
 cp_survival <- function(df, age, deaths, pop) {
-  return(1 - (deaths/(pop + 0.5*deaths)))
+  df <- df %>%
+    mutate(cp_survival = 1 - (deaths/(pop + 0.5*deaths)))
 }
 
-
+#' Number surviving to age x
+#'
+#' @param df
+#' @param age
+#' @param deaths
+#' @param pop
+#' @param cp_death
+#'
+#' @return
+#' @export
+#'
+#' @examples
+survive_to_age <- function(df, age, deaths, pop, ...) {
+  df <- df %>%
+    mutate(surviving_to_age_x = ifelse(row_number() == 0, 100000, cp_survival*lag(survive_to_age(df, age, deaths, pop))))
+}
 
 #' Making the lifetable
 #'
@@ -52,14 +69,16 @@ cp_survival <- function(df, age, deaths, pop) {
 #' @param pop
 #' @param deaths
 #'
-#' @return
+#' @return lifetable
 #' @export
 #' @import dplyr
 #' @examples
-makes_lifetable <- function(df, age, pop, deaths) {
+makes_lifetable <- function(df, age, deaths, pop) {
   lifetable <- df %>%
-    mutate(death_rate = death_rate(),
-           cp_death = cp_death(),
-           cp_survival = cp_survival())
+    mutate(death_rate = death_rate(df, age, deaths, pop),
+           cp_death = cp_death(df, age, deaths, pop),
+           cp_survival = cp_survival(df, age, deaths, pop),
+           survive_to_age = survive_to_age(df, age, deaths, pop))
+  return(lifetable)
 }
 
