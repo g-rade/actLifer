73d34a91468e0b34dac65fee3515a8854e90c95a
jtingy <jting@smith.edu>
2022-11-21 11:51:39 -0500

    Getting rid of observations with NA population

--- data-raw/DATASETS.R
+++ data-raw/DATASETS.R
@@ -18,7 +18,8 @@ mortality2 <- read.delim("data-raw/mortality2016_2.txt") %>%
   select(age_group, deaths, population) %>%
   filter(!is.na(deaths)) %>%
   mutate(age_group = ifelse(age_group == "", "Not Stated", age_group)) %>%
-  filter(age_group != "Not Stated", deaths != "Not Applicable", population != "Not Applicable")
+  filter(age_group != "Not Stated", deaths != "Not Applicable", population != "Not Applicable") %>%
+  mutate(is.na(population) != FALSE)
 
 mortality2$deaths <- as.numeric(mortality2$deaths)
 mortality2$population <- as.numeric(mortality2$population)
