1add269511acd67ae9c1d3e730f83d8be15c1107
mctp546 <mtylerpenny@smith.edu>
2022-11-22 09:52:29 -0500

    11/22: edit to testthat (and old functions deleted)

--- tests/testthat/test-apples.R
+++ tests/testthat/test-apples.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(nrow(mortality), 13)
+test_that("central_death_rate works", {
+  expect_equal(length(mortality2$CentralDeathRate), length(mortality2$age_group))
+})
+
+test_that("conditional_death_prob works", {
+  expect_equal(mortality_new$ConditionalProbDeath[1], 0.005816824892)
 })
