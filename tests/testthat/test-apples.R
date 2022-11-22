09b2843896006bdb0517e0fa34c3f54171952bb6
mctp546 <mtylerpenny@smith.edu>
2022-11-22 10:14:45 -0500

    11/22: edit test_apples

--- tests/testthat/test-apples.R
+++ tests/testthat/test-apples.R
@@ -1,9 +1,9 @@
 test_that("central_death_rate works", {
-  mortality2 <- central_death_rate(mortality2, age_group, population, deaths)
-  expect_equal(length(mortality2$CentralDeathRate), length(mortality2$age_group))
+  mortality3 <- central_death_rate(mortality2, age_group, population, deaths)
+  expect_equal(length(mortality3$CentralDeathRate), length(mortality3$age_group))
 })
 
 test_that("conditional_death_prob works", {
   mortality_new <- conditional_death_prob(mortality2, age_group, population, deaths)
-  expect_equal(mortality2$ConditionalProbDeath[1], 0.005816824892)
+  expect_equal(mortality_new$ConditionalProbDeath[1], 0.005816824892)
 })
