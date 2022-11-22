99ea4345c7d99b95b738b664286e9ad77e694ff6
g-rade <grade@smith.edu>
2022-11-22 10:47:10 -0500

    11/22/2022: making the datasets into tibbles

--- data-raw/DATASETS.R
+++ data-raw/DATASETS.R
@@ -1,6 +1,7 @@
 library(janitor)
 library(dplyr)
 library(purrr)
+library(tibble)
 
 mortality <- read.delim("data-raw/mortality2016.txt") %>%
   select(Age.Group, Deaths, Population) %>%
@@ -8,9 +9,12 @@ mortality <- read.delim("data-raw/mortality2016.txt") %>%
   filter(!is.na(deaths)) %>%
   mutate(age_group = ifelse(age_group == "", "Not Stated", age_group)) %>%
   filter(age_group != "Not Stated",  deaths != "Not Applicable", population != "Not Applicable") %>%
-  mutate(deaths = as.numeric(deaths), population = as.numeric(population))
+  mutate(deaths = as.numeric(deaths), population = as.numeric(population)) %>%
+  as_tibble()
 
 usethis::use_data(mortality, overwrite = TRUE)
+
+
 
 mortality2 <- read.delim("data-raw/mortality2016_2.txt") %>%
   clean_names() %>%
@@ -24,5 +28,6 @@ mortality2 <- read.delim("data-raw/mortality2016_2.txt") %>%
 mortality2$deaths <- as.numeric(mortality2$deaths)
 mortality2$population <- as.numeric(mortality2$population)
 
+mortality2 <- as_tibble(mortality2)
 
 usethis::use_data(mortality2, overwrite = TRUE)
