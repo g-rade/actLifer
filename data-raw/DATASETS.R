84f837515b61d668ce10bb7f3b7608bf2d5122c3
g-rade <grade@smith.edu>
2022-11-29 10:28:03 -0500

    11/29/2022: making the datasets tibbles

--- data-raw/DATASETS.R
+++ data-raw/DATASETS.R
@@ -10,11 +10,8 @@ mortality <- read.delim("data-raw/mortality2016.txt") %>%
   mutate(age_group = ifelse(age_group == "", "Not Stated", age_group)) %>%
   filter(age_group != "Not Stated",  deaths != "Not Applicable", population != "Not Applicable") %>%
   mutate(deaths = as.numeric(deaths), population = as.numeric(population)) %>%
-<<<<<<< HEAD
   as_tibble()
-=======
-  as.tibble()
->>>>>>> 8c75b3a099d666ca8bfcf33f624f7156b889ab7d
+
 
 usethis::use_data(mortality, overwrite = TRUE)
 
@@ -27,12 +24,9 @@ mortality2 <- read.delim("data-raw/mortality2016_2.txt") %>%
   filter(!is.na(deaths)) %>%
   mutate(age_group = ifelse(age_group == "", "Not Stated", age_group)) %>%
   filter(age_group != "Not Stated", deaths != "Not Applicable", population != "Not Applicable") %>%
-<<<<<<< HEAD
-  filter(is.na(population) == FALSE)
-
-=======
+  filter(is.na(population) == FALSE) %>%
   as_tibble()
->>>>>>> 8c75b3a099d666ca8bfcf33f624f7156b889ab7d
+
 
 mortality2$deaths <- as.numeric(mortality2$deaths)
 mortality2$population <- as.numeric(mortality2$population)
@@ -48,7 +42,8 @@ mortality3 <- read.delim("data-raw/gender_mortality2016.txt") %>%
   filter(!is.na(deaths)) %>%
   mutate(age_group = ifelse(age_group == "", "Not Stated", age_group)) %>%
   filter(age_group != "Not Stated", deaths != "Not Applicable", population != "Not Applicable") %>%
-  filter(is.na(population) == FALSE)
+  filter(is.na(population) == FALSE) %>%
+  as_tibble()
 
 usethis::use_data(mortality3, overwrite = TRUE)
 
