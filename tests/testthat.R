ca8ed4306b480e77e82c0e65c03a750a70ce6103
g-rade <grade@smith.edu>
2023-11-14 14:48:15 -0500

    11/14/2023: updating the tests w/ the new name

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(lifetables)
+library(actLifer)
 
-test_check("lifetables")
+test_check("actLifer")
